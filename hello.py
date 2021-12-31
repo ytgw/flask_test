fde3460affc79b7be4bd41763955575046263d6f
ytgw <tagawa0920@gmail.com>
2022-01-01 00:31:25 +0900

    POSTリクエストに対するAPI例を追加。

--- hello.py
+++ hello.py
@@ -1,7 +1,22 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return 'login post'
+    else:
+        return 'login get'
+
+@app.route('/post_json', methods=['POST'])
+def post_json():
+    if request.method == 'POST':
+        json_data = request.get_json()
+        return {'key1': json_data['key1'], 'key2': json_data['key2']}
+    else:
+        return 'not post'

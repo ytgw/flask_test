609c00bbb71c6f844db9304f8208fa81a24353a1
ytgw <tagawa0920@gmail.com>
2022-01-01 02:54:36 +0900

    型ヒント関連の修正。

--- hello.py
+++ hello.py
@@ -1,19 +1,22 @@
-from flask import Flask, request
+from typing import Any, Dict
+from flask import Flask, request, abort
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/')     # type:ignore
 def index():
     return 'Index Page'
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])   # type:ignore
 def login():
-    if request.method == 'POST':
+    if request.method == 'POST':    # type:ignore
         return 'login post'
     else:
         return 'login get'
 
-@app.route('/post_json', methods=['POST'])
-def post_json():
+@app.route('/post_json', methods=['POST'])  # type:ignore
+def post_json() -> Dict[str, Any]:
     json_data = request.get_json()
+    if json_data is None:
+        abort(400)
     return {'key1': json_data['key1'], 'key2': json_data['key2']}
